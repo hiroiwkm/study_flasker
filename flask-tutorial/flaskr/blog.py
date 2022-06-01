f092e3d9d75a438842b56041297f5c5ee6361453
Hirono Yamaguchi <yamaguchi.h@x-point-1.net>
2022-06-01 23:16:14 +0900

    チュートリアル

---
+++ flask-tutorial/flaskr/blog.py
@@ -0,0 +1,108 @@
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, url_for
+)
+from werkzeug.exceptions import abort
+
+from flaskr.auth import login_required
+from flaskr.db import get_db
+
+bp = Blueprint('blog', __name__)
+
+# インデックスページ
+@bp.route('/')
+def index():
+    db = get_db()
+    posts = db.execute(
+        'SELECT p.id, title, body, created, author_id, username'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' ORDER BY created DESC'
+    ).fetchall()
+    return render_template('blog/index.html', posts=posts)
+
+# 新規作成
+@bp.route('/create', methods=('GET', 'POST'))
+@login_required #ログインしているかチェック
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        error = None
+
+        # タイトルは必須項目
+        if not title:
+            error = 'Title is required.'
+
+        if not title:
+            error = 'Title is required.'
+
+        # エラーがなければデータベースにかきこみ
+        else:
+            db = get_db()
+            db.execute(
+                'INSERT INTO post (title, body, author_id)'
+                ' VALUES (?, ?, ?)',
+                (title, body, g.user['id'])
+            )
+            db.commit()
+        return redirect(url_for('blog.index'))
+        
+    return render_template('blog/create.html')
+
+# 更新
+# ログインしているユーザと作者が一致しているかチェックする関数を先に定義している。
+def get_post(id, check_author=True):
+    post = get_db().execute(
+        'SELECT p.id, title, body, created, author_id, username'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' WHERE p.id = ?',
+        (id,)
+    ).fetchone()
+
+    # abort()はHTTPのステータスコードを返す特殊な例外を発生させます。
+    # エラーと一緒に表示されるメッセージをオプションで引き取り、そうでないときは標準設定のメッセージを使用。
+    if post is None:
+        abort(404, f"Post id {id} doesn't exist.")
+
+    if check_author and post['author_id'] != g.user['id']:
+        abort(403)
+
+    return post
+
+# 上の関数を使用してテータの更新をする
+@bp.route('/<int:id>/update', methods=('GET', 'POST')) #update関数はid引数を受け取る
+@login_required #ログインユーザーかチェック
+def update(id):
+    post = get_post(id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        error = None
+
+        if not title:
+            error = 'Title is required.'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'UPDATE post SET title = ?, body = ?'
+                ' WHERE id = ?',
+                (title, body, id)
+            )
+            db.commit()
+            return redirect(url_for('blog.index'))
+
+    return render_template('blog/update.html', post=post)
+
+
+# 消去
+@bp.route('/<int:id>/delete', methods=('POST',))
+@login_required
+def delete(id):
+    get_post(id)
+    db = get_db()
+    db.execute('DELETE FROM post WHERE id = ?', (id,))
+    db.commit()
+    return redirect(url_for('blog.index'))
